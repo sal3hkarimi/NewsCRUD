4355c3fe84a2125e121b8acb05336b4002d06622
salehkarimy <salehkarimi423@gmail.com>
2023-12-12 08:04:42 +0330

    clean code

--- CRUD/app/index.js
+++ CRUD/app/index.js
@@ -6,32 +6,6 @@ require("./boot");
 require("./middlewares")(app);
 require("./routes")(app);
 
-// app.get("/user", (req, res) => {
-//   User.findAll()
-//     .then((result) => {
-//       console.log(result);
-//       res.json(result);
-//     })
-//     .catch((err) =>
-//       res.json({
-//         status: false,
-//         message: err.message,
-//       })
-//     );
-// });
-
-// app.post("/user", (req, res) => {
-//   User.create(req.body)
-//     .then((result) => {
-//       console.log(result);
-//       res.json({
-//         status: true,
-//         ...req.body,
-//       });
-//     })
-//     .catch((err) => res.json({ status: false, message: err.message }));
-// });
-
 module.exports = () => {
   app.listen(port, () => {
     console.log("app running on port", port);
